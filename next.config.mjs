f23fab743a36f59404c47907b3398957392a5eef
Yassgafa <yassirfalco@hotmail.com>
2025-12-31 20:48:21 -0500

    adios 4

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/Adios' : '',
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production' ? '/Adios' : '',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
