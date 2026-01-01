8065541bb322bd54c3ff2f8cd8e3f954c83ffa8c
Yassgafa <yassirfalco@hotmail.com>
2025-12-31 20:12:25 -0500

    Updates

--- _next/static/6GIiKx1rYziPu2gyPfaEl/_buildManifest.js
+++ _next/static/6GIiKx1rYziPu2gyPfaEl/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/automatic-photo-banner//_next/:path+",
+        "destination": "/automatic-photo-banner/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
