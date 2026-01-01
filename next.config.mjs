1cd093500a61f9201bb802996f5cefc3d9cc7290
Yassgafa <yassirfalco@hotmail.com>
2025-12-31 21:21:35 -0500

    adios 6

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/Adios",
-  assetPrefix: "/Adios/",
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/Adios' : '',
+  env: {
+    NEXT_PUBLIC_BASE_PATH: process.env.NODE_ENV === 'production' ? '/Adios' : '',
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     unoptimized: true,
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
